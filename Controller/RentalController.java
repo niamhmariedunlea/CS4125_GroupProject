2de0e823f235c9a32a69a8de9c7cda21f85c6ad6
LyGabi <lykogabriela@gmail.com>
2023-11-01 13:46:05 +0000

    Rental controller added with the relevant packages

---
+++ Controller/RentalController.java
@@ -0,0 +1,56 @@
+package Controller;
+
+import java.sql.Date;
+
+import Model.Rental;
+
+public class RentalController {
+    private Rental model;
+    private RentalView view;
+
+    public int getRentalID()
+    {
+        return model.getUserID();
+    }
+
+    public void setRentalID(int rentalID)
+    {
+        model.setRentalID(rentalID);
+    }
+
+    public int getScooterID()
+    {
+        return model.getScooterID();
+    }
+
+    public void setScooterID(int scooterID)
+    {
+        model.setScooterID(scooterID);
+    }
+
+    public Date getStartDate()
+    {
+        return model.getStartDate();
+    }
+
+    public void setStartDate(Date startDate)
+    {
+        model.setStartDate(startDate);
+    }
+
+    public double getRentalCost()
+    {
+        return model.getRentalCost();
+    }
+
+    public void setRentalCost(double rentalCost)
+    {
+        model.setRentalCost(rentalCost);
+    }
+
+    public void updateView()
+    {
+        view.displayRentalInformation(model.getRentalID(), model.getScooterID(), model.getStartDate(), model.getRentalCost());
+    }
+    
+}
