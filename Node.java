4d275a00e90bbc3ebc253372d0dc6c455a75117e
LyGabi <lykogabriela@gmail.com>
2023-11-01 12:04:55 +0000

    Model classes with constructors and getter/setter

---
+++ Node.java
@@ -0,0 +1,65 @@
+import java.util.List;
+
+public class Node {
+
+    private int nodeID;
+    private String position;
+    private List<Scooter> availableScooters;
+
+
+
+    public Node(int nodeID, String position, List<Scooter> availableScooters)
+    {
+        this.nodeID = nodeID;
+        this.position = position;
+        this.availableScooters = availableScooters;
+    }
+
+    public int getNodeID()
+    {
+        return nodeID;
+    }
+
+    public void setNodeID(int nodeID)
+    {
+        this.nodeID = nodeID;
+    }
+
+    public String getPosition()
+    {
+        return position;
+    }
+
+    public void setPosition(String position)
+    {
+        this.position = position;
+    }
+
+    public List<Scooter> getAvailableScooters()
+    {
+        return availableScooters;
+    }
+
+    public void setAvailableScooters(List<Scooter> availableScooters)
+    {
+        this.availableScooters = availableScooters;
+    }
+
+    public Boolean rentScooter(int scooterID)
+    {
+        if () {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+
+    }
+
+    public void returnScooter(int scooterID)
+    {
+
+    }
+    
+}
---
+++ Rental.java
@@ -0,0 +1,120 @@
+import java.sql.Date;
+import java.util.concurrent.TimeUnit;
+import java.lang.Math;
+
+public class Rental {
+
+    private int rentalID, userID, scooterID;
+
+    private boolean paid;
+
+    private Date startDate;
+    private Date endDate;
+
+    private double rentalCost;
+
+    public Rental(int rentalID, int userID, int scooterID, Date startDate, Date endDate, double rentalCost, boolean paid)
+    {
+        this.rentalID = rentalID;
+        this.userID = userID;
+        this.scooterID = scooterID;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.rentalCost = rentalCost;
+        this.paid = paid;
+    }
+
+    public int getRentalID()
+    {
+        return rentalID;
+    }
+
+    public void setRentalID(int rentalID)
+    {
+        this.rentalID = rentalID;
+    }
+
+    public int getUserID()
+    {
+        return userID;
+    }
+
+    public void setUserID(int userID)
+    {
+        this.userID = userID;
+    }
+
+    public int getScooterID()
+    {
+        return scooterID;
+    }
+
+    public void setScooterID(int scooterID)
+    {
+        this.scooterID = scooterID;
+    }
+
+    public Date getStartDate()
+    {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate)
+    {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate()
+    {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate)
+    {
+        this.endDate = endDate;
+    }
+
+    public double getRentalCost()
+    {
+        return rentalCost;
+    }
+
+    public void setRentalCost(double rentalCost)
+    {
+        this.rentalCost = rentalCost;
+    }
+
+    public boolean getPaid()
+    {
+        return paid;
+    }
+
+    public void setPaid(boolean paid)
+    {
+        this.paid = paid;
+    }
+
+    public static double calculateCost(Date startDate, Date endDate)
+    {
+        long start = startDate.getTime();
+        long end = endDate.getTime();
+
+        long timeDiff = Math.abs(end - start);
+        long daysDiff = TimeUnit.DAYS.convert(timeDiff, TimeUnit.MILLISECONDS);
+        double days = (double)daysDiff;
+
+        if (days > 7) 
+        {
+            double penalty = 25.00;
+            return penalty;
+        }
+        else
+        {
+            return 0.00;
+        }
+
+    }
+
+
+
+}
