c0490f013822340ac28bfff15772bbbf13ecabd2
LyGabi <lykogabriela@gmail.com>
2023-11-01 16:05:24 +0000

    Account Controller  - need to discuss the relationships here

---
+++ Controller/AccountController.java
@@ -0,0 +1,69 @@
+package Controller;
+
+import Model.Account;
+
+public class AccountController {
+    private Account model;
+    private AccountView view;
+
+    public AccountController(Account model, AccountView view)
+    {
+        this.model = model;
+        this.view = view;
+    }
+    
+    private String firstName;
+    private String lastName;
+
+    public int getUserID()
+    {
+        return model.getUserID();
+    }
+
+    public void setUserID(int userID)
+    {
+        model.setUserID(userID);
+    }
+
+    public String getUlEmail()
+    {
+        return model.getUlEmail();
+    }
+
+    public void setUlEmail(String ulEmail)
+    {
+        model.setUlEmail(ulEmail);
+    }
+
+    public String getPassword()
+    {
+        return model.getPassword();
+    }
+
+    public void setPassword(String password)
+    {
+        model.setPassword(password);
+    }
+
+    public String getFirstName()
+    {
+        return model.getFirstName();
+    }
+
+    public void setFirstName(String firstName)
+    {
+        model.setFirstName(firstName);
+    }
+
+    public String getLastName()
+    {
+        return model.getLastName();
+    }
+
+    public void setLastName(String lastName)
+    {
+        model.setLastName(lastName);
+    }
+
+    
+}
