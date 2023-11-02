1019a2dd5ac40b57cbcb763a9c271e1575709eaf
KamilJaba <kamiljablonski168@gmail.com>
2023-11-02 13:17:45 +0000

    More Boilerplates

---
+++ Controller/PaymentController.java
@@ -0,0 +1,19 @@
+package Controller;
+
+import Model.Payment;
+
+public class PaymentController {
+
+    private boolean paymentComplete;
+
+    public void makePayment() {
+        //api api api
+        //if(payment is valid) {
+            this.paymentComplete = true;
+        ///
+    }
+
+    public boolean isPaymentComplete() {
+        return this.paymentComplete;
+    }
+}
---
+++ Model/Payment.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Payment {
+
+    private double amount;
+    private boolean valid;
+
+    public double getMoney() {
+        return this.amount;
+    }
+    
+}
