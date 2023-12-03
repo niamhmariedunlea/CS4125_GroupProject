f472d330d29179b267699940f789616c57f513f9
niamhmariedunlea <21145059@studentmail.ul.ie>
2023-12-03 21:25:24 +0000

    changes to decorator

--- Model/BatteryWarningDecorator.java
+++ Model/BatteryWarningDecorator.java
@@ -3,17 +3,21 @@ package Model;
 public class BatteryWarningDecorator extends ScooterDecorator{
     private int warningBatteryLevel;
 
-public BatteryWarningDecorator(Scooter decoratedScooter, int warningBatteryLevel){
-    super(decoratedScooter);
-    this.warningBatteryLevel = warningBatteryLevel;
+    public BatteryWarningDecorator(Scooter decoratedScooter, int warningBatteryLevel){
+        super(decoratedScooter);
+        this.warningBatteryLevel = warningBatteryLevel;
+    }
+
+    public void startRental(){
+        super.startRental();
+        System.out.println("Battery level is now: " + batteryLevel);
+    }
+
+    public void endRide(){
+        super.endRide(); // call method in ScooterDecorator 
+    }
 }
 
-public void startRental(){
-    super.startRental();
-    System.out.println("Battery level is now: " + batteryLevel);
-}
 
-public void endRide(){
-    super.endRide(); // call method in ScooterDecorator 
-}
-}
+
+
